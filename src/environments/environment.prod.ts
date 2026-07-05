d594a8305d0694dcee91fb8acd9e58a1d74ad9b2
Sudhin S <sudhin95@gmail.com>
2026-07-05 23:15:47 +0530

    changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,14 +1,14 @@
     export const environment = {
   production: false,
 
-  apiUrl: 'https://quotationbackend.onrender.com/',
-  //  apiUrl: 'http://localhost:5200/',
+  // apiUrl: 'https://quotationbackend.onrender.com/',
+   apiUrl: 'http://localhost:5200/',
 
   appName: 'Quotation Management System',
 
   enableLogs: true,
 
-  loginApiUrl: 'api/v1/login',
+  loginApiUrl: 'api/v1/auth',
   clientsApiUrl: 'api/v1/clients',
   quotationsApiUrl: 'api/v1/quotations',
   aiLogsApiUrl: 'api/v1/ai-logs'
